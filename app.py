5c410a1afcd2a032f5212f51732adb6eb7e30644
athang12 <athangyende459@gmail.com>
2024-02-28 02:25:56 +0530

    ls

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ app = Flask( __name__ )
 def index():
     return "Hello Athang"
 
-if __name__ == '__main__':
-    app.run(debug=True, threaded=True)
+# if __name__ == '__main__':
+#     app.run(debug=True, threaded=True)
